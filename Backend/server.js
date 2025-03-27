fe2345f0006d007dcdb6153ad90b91b1ac72f2ac
Sahil Gupta <guptasahil2175@gmail.com>
2025-03-27 20:08:19 +0530

    Backend folder : Download page add

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const path = require('path');
 const connectDB = require('./config/db');
 
 // Connect to the database
 connectDB();
 
+// Template engine
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, './public')));
+
 // Routes
 app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`);
